05822c21887a4fec5e229b8e9c2787afc79efcd5
Jon <lundbyjon@hotmail.com>
2023-05-07 00:16:41 +0200

    1, 2, 3 solved

--- environments/environment-00/script.js
+++ environments/environment-00/script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+window.addEventListener("load", startApp);
+
+function startApp() {
+    document.querySelector("#result_failure").classList.add("hide");
+    document.querySelector("#result_success").classList.add("hide")
+    document.querySelector("#btn-knap").addEventListener("click", clicked);
+}
+
+function clicked() {
+    console.log("Det virker!");
+    document.querySelector("#result_failure").classList.add("hide");
+    document.querySelector("#result_success").classList.add("show")
+}
