ccac800e9ad8851041c267e8049aa7c1a0edcdbc
Jon <lundbyjon@hotmail.com>
2023-05-07 00:55:49 +0200

    123 solved

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,30 @@
 "use strict";
 
+window.addEventListener("load", startApp);
+
+async function startApp() {
+  const userList = await getUsers();
+
+  console.log(userList);
+
+  userList.forEach(showUser);
+}
+
+async function getUsers() {
+  const response = await fetch(`users.json`);
+  const data = await response.json();
+
+  return data;
+}
+
+function showUser(user) {
+  const html = /*html*/ `
+            <li>${user.name}: ${user.active}</li>
+    `;
+
+  document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
+
+  if (user.role === "admin") {
+    document.querySelector("#admin-count").insertAdjacentHTML("beforeend", html);
+  }
+}
