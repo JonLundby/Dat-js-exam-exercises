ef90f58c1dd6221be3c6fa488341fb9a57c98401
Jon <lundbyjon@hotmail.com>
2023-05-30 14:06:09 +0200

    Første forsøg. Løst?

--- environments/environment-06/script.js
+++ environments/environment-06/script.js
@@ -1,1 +1,53 @@
 "use strict";
+
+window.addEventListener("load", startApp);
+
+let itemsList = [];
+
+let basket = [];
+
+let itemsCount = 0;
+let itemsPrice = 0;
+let itemsWeight = 0;
+
+async function startApp() {
+  itemsList = await getItems();
+
+  console.log(itemsList);
+
+  itemsList.forEach(displayItems);
+}
+
+async function getItems() {
+  const response = await fetch(`products.json`);
+  const data = await response.json();
+
+  return data;
+}
+
+function displayItems(item) {
+  const html = /*html*/ `
+        <article>
+            <h3>NAME: ${item.name}</h3>
+            <p>vægt: ${item.weight}</p>
+            <p>pris: ${item.price},-</p>
+            <button>Læg i kurv</button>
+        </article>
+    `;
+
+  document.querySelector("#products").insertAdjacentHTML("beforeend", html);
+  document.querySelector("article:last-child button").addEventListener("click", addToBasket);
+
+  function addToBasket() {
+    basket.push(item);
+
+    itemsCount++;
+    document.querySelector("#total-in-basket").textContent = Number(itemsCount);
+
+    itemsPrice += item.price;
+    document.querySelector("#total-products").textContent = Number(itemsPrice);
+
+    itemsWeight += item.weight;
+    document.querySelector("#total-weight").textContent = Number(itemsWeight);
+  }
+}
