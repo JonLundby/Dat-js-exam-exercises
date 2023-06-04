2ab0694319ede52741e8db6e00b04b7cffbe93cc
Jon <lundbyjon@hotmail.com>
2023-06-04 23:51:58 +0200

    Solved. Problem with script modules and devtools.

--- environments/environment-04/script.js
+++ environments/environment-04/script.js
@@ -1,1 +1,42 @@
 "use strict";
+
+import { teachers } from "./teachers.js";
+
+window.addEventListener("load", startApp);
+
+function startApp() {
+    showTeachers();
+    createTeacher("jon", "jon@email.com")
+}
+
+function showTeachers() {
+    document.querySelector("#teachers-list").innerHTML = "";
+
+    for (const teacher of teachers) {
+        const htmlTeacher = /*html*/ `
+                        <li>${teacher.name} </br> ${teacher.email}</li>
+                        </br>
+        `;
+
+        document.querySelector("#teachers-list").insertAdjacentHTML("beforeend", htmlTeacher);
+    }
+}
+
+function createTeacher(name, email) {
+    // const name = name;
+    // const email = email;
+
+    const newTeacher = {
+        name: name,
+        email: email
+    };
+
+    teachers.push(newTeacher);
+
+    showTeachers();
+}
+
+function sayHello(name) {
+
+    console.log("hello" + name)
+}
